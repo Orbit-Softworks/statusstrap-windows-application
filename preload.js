0687d72294c6310f04862a7456db77515222d9b5
Smilez <smilezreal@yahoo.com>
2025-12-06 16:16:41 -0500

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,4 +1,3 @@
-// preload.js
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
@@ -23,9 +22,22 @@ contextBridge.exposeInMainWorld('electron', {
     
     // Update methods
     sendDiscordNotification: (title, oldVersion, newVersion, description, platform, image, isPrerelease) => 
-        ipcRenderer.send('send-discord-notification', title, oldVersion, newVersion, description, platform, image, isPrerelease),
+        new Promise((resolve) => {
+            ipcRenderer.once('discord-notification-sent', (event, success) => {
+                resolve(success);
+            });
+            ipcRenderer.send('send-discord-notification', title, oldVersion, newVersion, description, platform, image, isPrerelease);
+        }),
     checkUpdates: () => new Promise((resolve) => {
         ipcRenderer.once('update-check-completed', () => resolve());
         ipcRenderer.send('check-updates');
-    })
+    }),
+    
+    // Event listeners for bootstrapper updates
+    onBootstrapperUpdated: (callback) => {
+        ipcRenderer.on('bootstrapper-updated', (event, data) => callback(data));
+    },
+    
+    // App info
+    getAppVersion: () => require('electron').ipcRenderer.sendSync('get-version')
 });
